ccf0fee86e50c3649e86d5c7e10d4afb76d23637
Jasonljy90 <79128355+Jasonljy90@users.noreply.github.com>
2022-12-05 19:25:16 +0800

    initial commit

---
+++ src/MultiUserShoppingCart.java
@@ -0,0 +1,11 @@
+public class MultiUserShoppingCart {
+
+    public static void main(String[] args) {
+        
+        ShoppingCartDB cart = new ShoppingCartDB("cartdb"); // default folder: "db"
+        // cart.setup();
+        cart.startShell();
+        
+    }
+    
+}
